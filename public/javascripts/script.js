e2422c50ed20f30531467dc9b286387706247438
psophielam <psophielam92@gmail.com>
2020-10-05 00:09:00 +0200

    first version homepage done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+//FIXED SCROLL NAV BAR WITH TRANSITION
+// $(document).ready(function() {
+
+//   $(window).scroll(function() {
+
+//       var height = $('header').height();
+//       var scrollTop = $(window).scrollTop();
+
+//       if (scrollTop >= height - 60) {
+//           $('.navcontainer').addClass('solid-nav');
+//       } else {
+//           $('.navcontainer').removeClass('solid-nav');
+//       }
+
+//   });
+// });
